19c8e592014d7720f65a65208fa3376de05a71ba
RomikBobik <artemaremyc610@gmail.com>
2024-01-31 18:31:05 +0300

    хз чё это

--- les37/project01.cpp
+++ les37/project01.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 
 using namespace std;
+int sum(int a, int b);
 
 int main() {
 	int l;
 
 	cout << "Bo 123321 @_@";
+	cout << "Bo 123321 @_@";
+	cout << "Bo 123321 @_@";
+	cout << "Bo 123321 @_@";
+	cout << "Bo 123321 @_@";
+	cout << "Bo 123321 @_@"; cout << "Bo 123321 @_@";
+	cout << "Bo 123321 @_@";
+
 	cout << "OOO";
-	cin >> l;
+	cout << sum(1, 22) << endl;
 
 	return 0;
 }
